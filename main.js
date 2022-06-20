de6b283ff9d2d38f734ce2ef7eea5b209ca38ca9
YapikIsYarik <99668047+YapikIsYarik@users.noreply.github.com>
2022-06-20 17:25:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container')
+function randomColor() {
+   return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+}
+function generateBlocks() {
+   for (let i = 0; i < 25; i++) {
+      let square = document.createElement('div');
+      square.classList.add('square')
+      square.style.backgroundColor = randomColor();
+      container.appendChild(square)
+   }
+}
+
+function generateBlocksInterval() {
+   generateBlocks()
+   setInterval(() => {
+      document.querySelectorAll('.square').forEach(function (node) {
+         node.style.backgroundColor = randomColor();
+      });
+   }, 1000)
+}
+generateBlocksInterval();
+
